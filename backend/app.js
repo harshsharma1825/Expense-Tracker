f0570fc79f34ef19a77a812647e8662d35245321
Harsh <harshhs1238@gmail.com>
2024-09-12 15:43:34 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./DB/Database.js";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import transactionRoutes from "./Routers/Transactions.js";
+import userRoutes from "./Routers/userRouter.js";
+import path from "path";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT;
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Router
+app.use("/api/v1", transactionRoutes);
+app.use("/api/auth", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server is listening on http://localhost:${port}`);
+});
